efd5f21f8deb4e44f8af6850bdebf36c53ad1368
rotgruengelb <rotgrungelblab@gmail.com>
2023-03-03 22:16:29 +0100

    v2.0 music (lavalink)

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ sys.path.insert(0, package_dir)
 import discord
 from discord.ext import commands
 import nacl
+from gtts import gTTS
+import wavelink
+
+bot = discord.Bot()
+
+async def connect_nodes():
+    """Connect to our Lavalink nodes."""
+    await bot.wait_until_ready() # wait until the bot is ready
+
+    await wavelink.NodePool.create_node(
+        bot=bot,
+        host='localhost',
+        port=2333,
+        password='youshallnotpass'
+    ) # create a node
+
 
 bot = discord.Bot()
 connections = {}
@@ -57,5 +73,32 @@ async def stop_recording(ctx):
         await ctx.delete()  # And delete.
     else:
         await ctx.respond("I am currently not recording here.")  # Respond with this if we aren't recording.
+
+@bot.slash_command(name="play")
+async def play(ctx, search: str):
+    vc = ctx.voice_client # define our voice client
+
+    if not vc: # check if the bot is not in a voice channel
+        vc = await ctx.author.voice.channel.connect(cls=wavelink.Player) # connect to the voice channel
+
+    if ctx.author.voice.channel.id != vc.channel.id: # check if the bot is not in the voice channel
+        return await ctx.respond("You must be in the same voice channel as the bot.") # return an error message
+
+    song = await wavelink.YouTubeTrack.search(query=search, return_first=True) # search for the song
+
+    if not song: # check if the song is not found
+        return await ctx.respond("No song found.") # return an error message
+
+    await vc.play(song) # play the song
+    await ctx.respond(f"Now playing: `{vc.source.title}`") # return a message
+
+@bot.event
+async def on_ready():
+    await connect_nodes() # connect to the server
+
+@bot.event
+async def on_wavelink_node_ready(node: wavelink.Node):
+    print(f"{node.identifier} is ready.") # print a message
+
 
 bot_run(token_location=r"C:\Users\Daniel\envirorment\secrets\Galactic Hyperdrive\token.txt")
